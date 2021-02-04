bbd25f949b293e3b63d7682bc90d7cbcb428b816
Fatma Ocal <contactfatma@gmail.com>
2021-02-03 20:39:25 -0800

    update to postgres

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,9 +1,4 @@
 class PortfolioController < ApplicationController
   def index
   end
-
-  def pdf
-    pdf_filename = File.join(Rails.root, "portfolio/app/assets/resume_fatmaocal.pdf")
-    send_file(pdf_filename, :filename => "your_document.pdf", :disposition => 'inline', :type => "application/pdf")
-  end
 end
